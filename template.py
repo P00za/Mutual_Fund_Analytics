b9e5452dcaa302f071837dd478eae00d8a541efd
P00za <poozasingh0406@gmail.com>
2026-06-25 15:12:59 +0530

    Project  setup: Created folder structures

---
+++ template.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import os
+import logging
+
+# logging string
+logging.basicConfig(level=logging.INFO, format='%(asctime)s : %(levelname)s : %(message)s:')
+
+Project = "BlueStock Tasks"
+
+# List of folders to create
+folders = [
+    f"data/raw",
+    f"data/processed",
+    f"notebooks",
+    f"sql",
+    f"dashboard",
+    f"reports"
+]
+
+
+# Create folders
+for folder in folders:
+    folder = Path(folder)
+    folder.mkdir(parents=True, exist_ok=True)
+    logging.info(f"Ensured folder exists: {folder}")
+
+print("Project folder structure created successfully!")
